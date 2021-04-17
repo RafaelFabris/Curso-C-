2a05c007fca18f991c66590c464c3ef3576dceeb
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-04-17 04:46:24 -0300

    Tratamento de exceçoes

---
+++ Exceptions1/Exceptions1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using Exceptions1.Entities;
+using Exceptions1.Entities.Exceptions;
+
+namespace Exceptions1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double inicialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine() , CultureInfo.InvariantCulture);
+
+            Account acc = new Account(number , holder,  inicialBalance , withdrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                acc.Withdraw(amount);
+                Console.WriteLine("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error :"+     e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unexpected Error: " + e.Message);
+            }
+
+        }
+    }
+}
