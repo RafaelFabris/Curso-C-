40c22fb191beb05f555f5f5221abd857bed2bac9
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-07-23 11:42:03 -0300

    Directory

---
+++ Directory_DirectoryInfo/Directory_DirectoryInfo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Directory_DirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string path = @"c:\temp\myfolder";
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*" , SearchOption.AllDirectories);
+                Console.WriteLine("Folder");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Files");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory(path + "\\newFolder");
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An Error Occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
