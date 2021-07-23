87a6087524d1ea9e1c8b0ac14ec8eeec4d5e6638
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-07-23 16:50:30 -0300

    Exercicios de Fixacao de Arquivos

---
+++ Files1/Files1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using Files1.Entities;
+
+namespace Files1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter file full path: ");
+            string sourcePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourcePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\sumary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = new StreamWriter(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product product = new Product(name, price, quantity);
+
+                        sw.WriteLine(product.Name + "," + product.total().ToString("F2" , CultureInfo.InvariantCulture));
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
