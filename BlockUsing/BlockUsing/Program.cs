73ec0175e6839b50b7ca65a5d9cc9705f54f5ce5
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-07-23 11:17:33 -0300

    Files FileInfo, FileStream and StreamReady

---
+++ BlockUsing/BlockUsing/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace BlockUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+
+            /*try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }*/
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An Error Occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
