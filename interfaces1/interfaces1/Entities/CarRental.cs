6ce3c84b24388eb6faf0879e2e0632f35086d5e3
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-07-24 12:38:21 -0300

    Interfaces Parte 1

---
+++ interfaces1/interfaces1/Entities/CarRental.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace interfaces1.Entities
+{
+    class CarRental
+    {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+
+        public Vehicle vehicle { get; set; }
+
+        public Invoice invoice { get; set; }
+
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            Start = start;
+            Finish = finish;
+            this.vehicle = vehicle;
+        }
+
+            
+    }
+}
---
+++ interfaces1/interfaces1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using interfaces1.Entities;
+using interfaces1.Services;
+
+namespace interfaces1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.WriteLine("Car model: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm" , CultureInfo.InvariantCulture);
+            Console.WriteLine("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Enter price per hour: ");
+            double priceHours = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter price per day: ");
+            double priceDays = double.Parse(Console.ReadLine());
+
+            
+            CarRental carRental = new CarRental(start , finish , new Vehicle(model));
+            RentalServic rentalServic = new RentalServic(priceHours, priceDays);
+            rentalServic.ProcessInvoice(carRental);
+            Console.WriteLine(carRental.invoice.ToString());
+
+        }
+    }
+}
