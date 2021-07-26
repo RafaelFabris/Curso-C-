93c13a09e864dd131d98d82de5ac1b700768966b
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-07-26 15:14:51 -0300

    Interface parte 3 - Exercicios de Fixação

---
+++ Interface2/Interface2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using Interface2.Entities;
+using Interface2.Services;
+
+namespace Interface2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.WriteLine("Number:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Date (dd/MM/yyyy):");
+            DateTime startDate = DateTime.ParseExact(Console.ReadLine() , "dd/MM/yyyy" , CultureInfo.InvariantCulture);
+            Console.WriteLine("Contract value:");
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Enter number of installments:");
+            int installments = int.Parse(Console.ReadLine());
+
+            Contract contract = new Contract(number,startDate,value);
+            ContractService contractService = new ContractService(new PaypalService());
+
+            contractService.ProcessContract(contract , installments);
+
+            Console.WriteLine("Installments: ");
+            foreach (Installment installment in contract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+            
+        }
+    }
+}
---
+++ Interface2/Interface2/Services/PaypalService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface2.Services
+{
+    class PaypalService : IOnlinePaymentService
+    {
+        private const double FeePayment = 0.02;
+        private const double MonthlyInterest = 0.01;
+
+        public double Interest(double amount, int months)
+        {
+            return amount * MonthlyInterest * months;
+        }
+
+        public double PaymentFee(double amount)
+        {
+            return amount * FeePayment;
+        }
+    }
+}
