3280f786004355623001525f1c66f9669def4a67
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-07-26 18:13:24 -0300

    IComparable

---
+++ Interface5/Interface5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Interface5.Entities;
+
+namespace Interface5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\names.csv";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach(Employee emp in list)
+                        Console.WriteLine(emp);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
