12ff14c1e412065151a7b722c0b62b81e799ca20
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-09-17 15:02:49 -0300

    Restricoes Generics, GetHashCode e Equals, HashSet e SortedHash

---
+++ Generics2/Generics2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Generics2.Services;
+using Generics2.Entities;
+using System.Globalization;
+
+namespace Generics2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            Console.Write("Enter N:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            for(int i = 0; i< n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1] , CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine();
+            Console.Write("Max: ");
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ Generics2/Generics2/Services/CalculationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics2.Services
+{
+    class CalculationService
+    {
+        public T Max<T>(List<T> list) where T :  IComparable
+        {
+            if(list.Count == 0)
+            {
+                throw new ArgumentException("The list can not be empty");
+            }
+
+            T max = list[0];
+            for(int i = 1; i< list.Count; i++)
+            {
+                if(list[1].CompareTo(max) > 0)
+                {
+                    max = list[1];
+                }
+            }
+            return max;
+        }
+    }
+}
