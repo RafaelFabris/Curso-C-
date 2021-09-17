10699701e3532a8d998f7c5f8e393473abf5767c
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-09-17 17:42:33 -0300

    Hash comparam Igualdade

--- HashSetESortedSet/HashSetESortedSet/Program.cs
+++ HashSetESortedSet/HashSetESortedSet/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using HashSetESortedSet.Entities;
 
 namespace HashSetESortedSet
 {
@@ -7,47 +8,19 @@ namespace HashSetESortedSet
     {
         static void Main(string[] args)
         {
-            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
-            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV" , 900.0));
+            a.Add(new Product("Notebook", 1200.0));
 
-            // Union
-            SortedSet<int> c = new SortedSet<int>(a);
-            c.UnionWith(b);
-            PrintCollection(c);
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
 
-            // intersection
-            SortedSet<int> d = new SortedSet<int>(a);
-            d.IntersectWith(b);
-            PrintCollection(d);
+            Product prod = new Product("Notebook" , 1200.0);
+            Console.WriteLine(a.Contains(prod));
 
-            // difference
-            SortedSet<int> e = new SortedSet<int>(a);
-            e.ExceptWith(b);
-            PrintCollection(e);
-
-            //HashSet<string> set = new HashSet<string>();
-
-            /*
-            set.Add("TV");
-            set.Add("Notebook");
-            set.Add("Tablet");
-
-            Console.WriteLine(set.Contains("Notebook"));
-
-            foreach  (string p in set)
-            {
-                Console.WriteLine(p);
-            }
-            */
-        }
-
-         static void PrintCollection<T>(IEnumerable<T> collection)
-        {
-            foreach (T obj in collection)
-            {
-                Console.Write(obj + " ");
-            }
-            Console.WriteLine();
+            Point point = new Point(3, 4);
+            Console.WriteLine(b.Contains(point));
         }
     }
 }
