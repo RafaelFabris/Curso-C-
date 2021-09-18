912d314c23985e2c3bf5d28ed65c5109dbd164df
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-09-18 17:15:49 -0300

    ExtensionMethod

---
+++ ExtensionMethod/ExtensionMethod/Extension/DateTimeExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace System
+{
+    static class DateTimeExtension
+    {
+        public static string ElapsedTime(this DateTime thisobject)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisobject);
+
+            if (duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " Hours";
+            }
+            else
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " Days";
+        }
+    }
+}
---
+++ ExtensionMethod/ExtensionMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2020, 9, 18, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Hello World";
+            Console.WriteLine(s1.Cut(5));
+;        }
+    }
+}
