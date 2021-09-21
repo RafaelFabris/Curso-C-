4b88f8cf072c07e36ca01296f773b84a675b1bf4
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-09-21 18:05:45 -0300

    Linq 4

--- Linq1/Linq1/Entities/Product.cs
+++ Linq1/Linq1/Entities/Product.cs
@@ -9,22 +9,24 @@ namespace Linq1.Entities
 {
     class Product
     {
-        public int Id { get; set; }
+        
         public string Name { get; set; }
         public double Price { get; set; }
-        public Category Category { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
 
         public override string ToString()
         {
-            return Id
+            return
+
+                Name
                 + ", "
-                + Name
-                + ", "
-                + Price.ToString("F2" , CultureInfo.InvariantCulture )
-                + ", "
-                + Category.Name
-                + ", "
-                + Category.Tier;
+                + Price.ToString("F2", CultureInfo.InvariantCulture);
+                
         }
     }
 }
--- Linq1/Linq1/Program.cs
+++ Linq1/Linq1/Program.cs
@@ -2,164 +2,41 @@
 using System.Linq;
 using Linq1.Entities;
 using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
 
 namespace Linq1
 {
     class Program
     {
-        static void Print<T>(string message, IEnumerable<T> collection)
-        {
-            Console.WriteLine(message);
-            foreach (T obj in collection)
-            {
-                Console.WriteLine(obj);
-            }
-            Console.WriteLine();
-        }
-
         static void Main(string[] args)
         {
-            Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
-            Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1 };
-            Category c3 = new Category() { Id = 3, Name = "Eletronics", Tier = 1 };
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
 
-            List<Product> products = new List<Product>()
+            List<Product> list = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
             {
-                new Product() { Id = 1, Name = "Computer", Price = 1100.0, Category = c2 },
-                new Product() { Id = 2, Name = "Hammer", Price = 90.0, Category = c1 },
-                new Product() { Id = 3, Name = "Tv", Price = 1700.0, Category = c3 },
-                new Product() { Id = 4, Name = "Notebook", Price = 1300.0, Category = c2 },
-                new Product() { Id = 5, Name = "Saw", Price = 80.0, Category = c1 },
-                new Product() { Id = 6, Name = "Tablet", Price = 700.0, Category = c2 },
-                new Product() { Id = 7, Name = "Camera", Price = 700.0, Category = c3 },
-                new Product() { Id = 8, Name = "Printer", Price = 350.0, Category = c3 },
-                new Product() { Id = 9, Name = "MacBook", Price = 1800.0, Category = c2 },
-                new Product() { Id = 10, Name = "Sound Bar", Price = 700.0, Category = c3 },
-                new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
-            };
-
-            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
-            var r1 =
-                from p in products
-                where p.Category.Tier == 1 && p.Price < 900.0
-                select p;
-            Print("Tier 1 AND PRICE < 900.0:", r1);
-
-            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
-            var r2 =
-                from p in products
-                where p.Category.Name == "Tools"
-                select p.Name;
-            Print("Names of PRODUCTS FROM TOOLS", r2);
-
-            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
-            var r3 =
-                from p in products
-                where p.Name[0] == 'C'
-                select new
+                while (!sr.EndOfStream)
                 {
-                    p.Name,
-                    p.Price,
-                    CategoryName = p.Category.Name
-                };
-            Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
-
-            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
-            var r4 =
-                from p in products
-                where p.Category.Tier == 1
-                orderby p.Name
-                orderby p.Price
-                select p;
-            Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
-
-            //var r5 = r4.Skip(2).Take(4);
-            var r5 =
-                (from p in r4
-                 select p).Skip(2).Take(4);
-            Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
-
-            //var r6 = products.FirstOrDefault();
-            var r6 = (from p in products select p).FirstOrDefault();
-            Console.WriteLine("FIRST or  DEFAULT TEST1: " + r6);
-
-            //var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
-            var r7 =
-                (from p in products
-                 where p.Price > 3000.0
-                 select p).FirstOrDefault();
-            Console.WriteLine("FISRT or DEFAULT TEST2:  " + r7);
-            Console.WriteLine();
-
-            //var r8 = products.Where(p => p.Id == 3).SingleOrDefault();
-            var r8 =
-                (from p in products
-                 where p.Id == 3
-                 select p).SingleOrDefault();
-            Console.WriteLine("SINGLE OR DEFAULT TEST1: " + r8);
-
-            //var r9 = products.Where(p => p.Id == 30).SingleOrDefault();
-            var r9 =
-                (from p in products
-                 where p.Id == 30
-                 select p).SingleOrDefault();
-            Console.WriteLine("SINGLE OR DEFAULT TEST2: " + r9);
-            Console.WriteLine();
-
-            //var r10 = products.Max(p => p.Price);
-            var r10 =
-                (from p in products
-                 select p.Price).Max();
-            Console.WriteLine("MAX PRICE: " + r10);
-
-            //var r11 = products.Min(p => p.Price);
-            var r11 =
-            (from p in products
-             select p.Price).Min();
-            Console.WriteLine("MIN PRICE: " + r11);
-
-            //var r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
-            var r12 =
-                (from p in products
-                 where p.Category.Id == 1
-                 select p.Price).Sum();
-            Console.WriteLine("Category 1 Sum Price: " + r12);
-
-            //var r13 = products.Where(p => p.Category.Id == 1).Average(p => p.Price);
-            var r13 =
-                (from p in products
-                 where p.Category.Id == 1
-                 select p.Price).Average();
-            Console.WriteLine("Category 1 Average Price: " + r13);
-
-            //var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
-            var r14 =
-                (from p in products
-                 where p.Category.Id == 5
-                 select p.Price).DefaultIfEmpty(0.0).Average();
-            Console.WriteLine("Category 5 Average Price: " + r14);
-
-            // var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
-            var r15 =
-                (from p in products
-                 where p.Category.Id == 1
-                 select p.Price).Aggregate(0.0, (x, y) => x + y);
-            Console.WriteLine("Category 1 Aggregate SUM: " + r15);
-
-            //var r16 = products.GroupBy(p => p.Category);
-            var r16 =
-                from p in products
-                group p by p.Category;
-            foreach (IGrouping<Category, Product> group in r16)
-            {
-                Console.WriteLine();
-                Console.WriteLine("Category " + group.Key.Name + ": ");
-                foreach (Product p in group)
-                {
-                    Console.WriteLine(p);
+                    string[] filds =  sr.ReadLine().Split(';');
+                    string name = filds[0];
+                    double price = double.Parse(filds[1],CultureInfo.InvariantCulture );
+                    list.Add(new Product(name, price));
                 }
-
             }
-        }
+
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Average price: " + avg.ToString("F2" , CultureInfo.InvariantCulture));
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p=>  p.Name).Select(p=> p.Name);
+
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }  
     }
 }
