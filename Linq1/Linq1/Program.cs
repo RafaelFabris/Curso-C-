2e28c6b4cba90302a8b883fb982b0a05319aa76a
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-09-21 15:06:06 -0300

    Linq 1

---
+++ Linq1/Linq1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            // Define the querry expression
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10 );
+
+            // Executet the querry
+            foreach(int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
